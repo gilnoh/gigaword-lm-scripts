fddb2d18e595f1e4cbb94504f0ba9ac033c8c467
Tae-Gil Noh <noh@cl.uni-heidelberg.de>
2013-01-25 21:27:40 +0100

    added the runner that processes a list of Gigaword GZ file into per news files

---
+++ gzset_runner.pl
@@ -0,0 +1,45 @@
+#!/usr/bin/perl 
+
+# This script uses gz_to_newsperfil.pl, to convert a set of 
+# GigaWord files into one 
+
+use warnings;
+use strict;
+use File::Basename; 
+
+my $SCRIPT_TO_RUN="gz_to_newsperfile.pl"; 
+my $CREATE_SUBDIRECTORY_AGENTYEAR=1; 
+   # if 1, subdirectory with the "year" generated 
+   # if 0, all in the output dir 
+my $OUTPUT_PATH_BASE="./output"; 
+die "needs a list of Gigaword gz files" unless ($ARGV[0]); 
+
+foreach(@ARGV)
+{
+    my $filename = $_; 
+    my $outpath;
+    if ($CREATE_SUBDIRECTORY_AGENTYEAR)
+    {
+	my $basename = fileparse($filename); 
+	$basename =~ /(.+)\d\d\./; 
+	my $agent_year = $1; 
+	$outpath = $OUTPUT_PATH_BASE . "/" . $agent_year; 
+	
+	unless (-d $outpath) 
+	{
+	    mkdir $outpath; 
+	}
+
+    }
+    else
+    {
+	$outpath = $OUTPUT_PATH_BASE; 
+    }
+
+    print STDERR "$filename  -to->  $outpath\n";
+    `perl $SCRIPT_TO_RUN $filename $outpath`; 
+}
+
+
+
+
