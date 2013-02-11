7ac5e93acd2dc66cf1f94fec1dbf961a866a3976
Tae-Gil Noh <noh@cl.uni-heidelberg.de>
2013-02-11 14:40:07 +0100

    added lambda sum for prob sequence

---
+++ lambda_sum.m
@@ -0,0 +1,10 @@
+function log_prob = lambda_sum(lambda, vectorL, vectorR)
+% Lamda sum on probability (number, not log prob) sequence 
+% this function gets two sequence of probabilities 
+% and sum them into a single log probability. 
+
+seq_prob = vectorL * lambda + vectorR * (1 - lambda); 
+log_prob = sum(log10(seq_prob));  
+
+% any of 0 in seq_prob will result in -INF. 
+% -INF is not removed in this function. Calling side should check this 
