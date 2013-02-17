d1e4193795899cad1baeb7a9876b3fc905661746
Tae-Gil Noh <noh@cl.uni-heidelberg.de>
2013-02-17 17:12:47 +0100

    added postprocessing for splitta last-period-stick problem

--- gigaword_split_file.pl
+++ gigaword_split_file.pl
@@ -7,7 +7,7 @@ use strict;
 use warnings; 
 
 # read 
-die "need an argument (file output from splitta with tokenizing)" unless ($ARGV[0]); 
+die "need two arguments. First: (file output from splitta with tokenizing), second: the output dir." unless ($ARGV[0]); 
 die "need the output dir (where each sentence splitted news story will be unpacked)" unless ($ARGV[1]); 
 die "unable to read file $ARGV[0]"  unless (-r $ARGV[0]); 
 die "unable to access directory $ARGV[1]" unless (-d $ARGV[1]); 
@@ -30,6 +30,16 @@ while(<FILEIN>)
 	open FILEOUT, ">", ($outputdir . "/" . $filename . "." . $ext) ; 
 
     }
+    # fixing tokenization error of Splitta (the end of sentence) 
+    # case 1) Period (\w.$) at the end  -> (\w .$) 
+    s/\.$/ \. /; 
+    # case 2) Period space quote (\w. " $) at the end. -> (\w . " $) 
+    s/\. " $/ \. " /;
+
+    # the above (seems to) work well for AFP, at least. 
+    # ALTERNATIVE: ... run splitta only for sentence split, 
+    # and run another tokenization runner. (NOT for the moment). 
+
     print FILEOUT lc($_); 
 }
 
