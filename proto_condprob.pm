88b9ed0454fd043ee89d40ba4662823385ae5f66
Tae-Gil Noh <noh@cl.uni-heidelberg.de>
2013-02-20 18:03:28 +0100

    trying to derive multithread P_t ... still working on

--- proto_condprob.pm
+++ proto_condprob.pm
@@ -8,22 +8,25 @@ use strict;
 use Exporter; 
 use srilm_call qw(read_debug3_p call_ngram); 
 use octave_call;
+use threads; 
 
 our @ISA = qw(Exporter); 
-our @EXPORT = qw(P_t); 
+our @EXPORT = qw(P_t P_t_multithread); 
 our @EXPORT_OK = qw (P_coll P_doc $COLLECTION_MODEL $DOCUMENT_MODEL_PATH); 
 
 # some constants 
 our $COLLECTION_MODEL = "./output/collection.model"; 
 our $DOCUMENT_MODELS = "./output/afp_eng_2009/*.model"; 
 our $LAMBDA = 0.5; 
+our $NUM_THREAD = 4; 
+
 
 my @collection_seq =(); # global variable that is filled by P_coll, and used by P_doc (thus in P_t)
 
-sub P_t($;$$$) 
+sub P_t_multithread($;$$$) 
 {
     # argument: text, lambda, collection model path, document model path 
-    # out: a hash? (model name, prob of given text produced from the model) 
+    # out: a hash (model name, prob of given text produced from the model) 
 
     my %result; # $result{"model_id"} = log prob of $text from 'model_id' 
 
@@ -53,15 +56,116 @@ sub P_t($;$$$)
     print STDERR $coll_logprob, "\n"; 
 
     # for each model, call P_doc()     
-    # ( may be extended with multi-threads, later ) 
+    print STDERR "Calculating per-document model logprobs, ", scalar(@document_model), " files\n"; 
+
+    # # divide the input 
+    # my @aref_array; 
+    # {
+    # 	my $n = int (scalar (@document_model) / $NUM_THREAD); 
+    # 	my $start = 0; 
+    # 	my $end = $n-1; 
+
+    # 	for(my $i=0; $i < ($NUM_THREAD - 1); $i++) # until last part 
+    # 	{
+    # 	    my @a = @document_model[$start .. $end]; 
+    # 	    $start += $n; 
+    # 	    $end = $start + $n - 1; 
+    # 	    push @aref_array, [@a];  
+    # 	}   
+    # 	# last part 
+    # 	$end = (scalar @document_model) - 1;  
+    # 	my @a = @document_model[$start .. $end]; 
+    # 	push @aref_array, [@a]; 
+
+    # 	#sanity check 
+    # 	die unless ((scalar (@aref_array)) == $NUM_THREAD); 
+    # 	my $sum =0; 
+    # 	foreach (@aref_array)
+    # 	{
+    # 	    $sum += scalar @{$_}; 
+    # 	}
+    # 	die unless (scalar(@document_model) == $sum); 
+    # }    
+    
+    # the models to be checked are divided in @aref_array 
+    # generate the threads, and run them. 
+    my @thread; 
+    my $n = int (scalar (@document_model) / $NUM_THREAD); 
+    my $start = 0; 
+    my $end = $n-1; 
+
+    for (my $i=0; $i < $NUM_THREAD; $i++)
+    {
+    	$thread[$i] = threads->create(\&P_d_runner, @document_model[$start .. $end]);
+    	# update next array
+    	$start += $n; 
+    	$end = $start + $n - 1; 
+    	$end = ((scalar @document_model) -1) if ($i == $NUM_THREAD -1); # last part special case
+    }
+
+    #%result = P_d_runner(@document_model); 
+
+
+    print STDERR  "\n"; 
+    return %result; 
+}
+
+sub P_d_runner
+{
+    # internal helper sub that is used by P_t_multithread. 
+    # gets a list of model files, call P_d on each of them. 
+    my %r; 
+    my $count = 0; 
+    foreach (@_)
+    {
+	$r{$_} = P_doc($_); 
+	print STDERR "." unless ($count++ % 100); 
+    }
+    return %r; 
+}
+
+
+sub P_t($;$$$) 
+{
+    # argument: text, lambda, collection model path, document model path 
+    # out: a hash (model name, prob of given text produced from the model) 
+
+    my %result; # $result{"model_id"} = log prob of $text from 'model_id' 
+
+    # argument copy & sanity check 
+    my $text = $_[0]; 
+    die unless ($text); 
+    if ($_[1]) { # lambda 
+	die unless ($_[1] >=0 and $_[1] <= 1); 
+	$LAMBDA = $_[1]; 
+    }
+    if ($_[2]) { # collection model (single file) 
+	die unless (-r $_[2]);
+	$COLLECTION_MODEL = $_[2]; 
+    }
+    if ($_[3]) { # document models as file glob string (e.g. "path/*.model") 
+	$DOCUMENT_MODELS = $_[3]; 
+    }
+
+    # get list of all document models     
+    my @document_model = glob($DOCUMENT_MODELS); 
+    die unless (scalar @document_model); 
+
+    # call P_coll() 
+    print STDERR "Calculating collection model logprob (to be interpolated)";  
+    my @r = P_coll($text); # return value already saved in global @collection_seq
+    my $coll_logprob = lambda_sum2(1, \@r, \@r); 
+    print STDERR $coll_logprob, "\n"; 
+
+    # for each model, call P_doc()     
     print STDERR "Calculating per-document model logprobs, ", scalar(@document_model), " files\n"; 
     my $count = 0; 
     foreach (@document_model)
     {
-	my $logprob = P_doc($_); 
-	$result{$_} = $logprob; 
+    	my $logprob = P_doc($_); 
+    	$result{$_} = $logprob; 
 	
-	print STDERR "." unless ($count++ % 100); 
+    	print STDERR "." unless ($count++ % 100); 
     }
     
     print STDERR  "\n"; 
--- sketch.pl
+++ sketch.pl
@@ -7,9 +7,10 @@ use octave_call;
 
 # test call on 2009 small set 
 
-my $text = "we can not say yet if there will be an agreement , \" said Merkel on her way into the summit .";  g
+my $text = "we can not say yet if there will be an agreement , \" said Merkel on her way into the summit .";  
 
-my %r = P_t($text); 
+#my %r = P_t($text); 
+my %r = P_t_multithread($text); 
 print "Done\n"; 
 export_hash_to_file(\%r, "Pt_from_sketch.txt"); 
 my @a = values %r;
