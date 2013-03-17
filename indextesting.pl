e28fdc5e90c2ac6c89cb994ffa22efcaf2a1c015
Tae-Gil Noh <noh@cl.uni-heidelberg.de>
2013-03-17 10:31:04 +0100

    now I understand how to work with Plucene index. Pity it isn't being actively maintained...

---
+++ indextesting.pl
@@ -0,0 +1,67 @@
+#!/usr/bin/perl
+
+# test run on plucene 
+
+use strict;
+use feature ":5.10";
+
+use Plucene; 
+use Plucene::Document; 
+use Plucene::Document::Field; 
+use Plucene::Search::IndexSearcher; 
+use Plucene::Analysis::SimpleAnalyzer; 
+use Plucene::Index::Writer; 
+use Plucene::QueryParser; 
+
+
+# indexing 
+# "UnStored" will not store the data, but index the data. 
+# "Text" will be indexed & stored. (thus retrived) 
+# "UnIndexed" will be stored, but not indexed (nor tokenized). 
+my $doc = Plucene::Document->new; 
+$doc->add(Plucene::Document::Field->UnIndexed(id => "afp_2010_1")); 
+$doc->add(Plucene::Document::Field->Text(text => "airline accident")); 
+#$doc->add(Plucene::Document::Field->UnStored(text => "airplane accident")); 
+
+my $doc2 = Plucene::Document->new; 
+$doc2->add(Plucene::Document::Field->UnIndexed(id => "afp_2010_2")); 
+$doc2->add(Plucene::Document::Field->UnStored(text => "airline strike")); 
+
+my $analyzer = Plucene::Analysis::SimpleAnalyzer->new();
+my $writer = Plucene::Index::Writer->new("my_index", $analyzer, 1);
+
+$writer->add_document($doc);
+$writer->add_document($doc2); 
+undef $writer; # close
+# die; 
+
+# prepare query
+my $parser = Plucene::QueryParser->new({
+    analyzer => Plucene::Analysis::SimpleAnalyzer->new(),
+    default  => "text" # Default field for non-specified queries
+				       });
+#my $query = $parser->parse('text:"strike"');
+#my $query = $parser->parse('text:"accident"');
+my $query = $parser->parse('text:"airline"'); 
+
+
+# search 
+my $searcher = Plucene::Search::IndexSearcher->new("my_index");
+
+my @docs;
+my $hc = Plucene::Search::HitCollector->new(collect => sub {
+    my ($self, $doc, $score) = @_;
+    push @docs, $searcher->doc($doc);
+					    
+					    });
+
+$searcher->search_hc($query => $hc);
+
+foreach my $doc (@docs)
+{
+    my Plucene::Document::Field @field = $doc->fields; 
+    foreach (@field)
+    {
+	print "name: ", $_->name()," \nstring: ", $_->string(), "\n"; 
+    }
+}
