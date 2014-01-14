1e71ca5c7db187fd3b897869396fd1e076bba1cd
Tae-Gil Noh <noh@cl.uni-heidelberg.de>
2014-01-14 14:15:55 +0100

    - improved rte3_runner. Now it is almost ready to do new experiments, with better lambda

--- rte3_runner.pl
+++ rte3_runner.pl
@@ -1,51 +1,48 @@
-# this simple script gets id number and runs  
-# P_h_t() over that problem. 
+# Simple runner that calculates P( hypo | text ) for RTE3 (EOP format)
+# XML file and report relevant values (like PMI-gain, PPL-gain,
+# probabilities, etc)
 
-# TODO update as getting "3-arguments". (would be faster) 
-# "filename", "from_id", "to_id". 
+# TODO? (BB value?  =  PPL( h | t ) / PPL(t / t) )
 
-# TODO fill PMI 
-# TODO (make minus as ppl-minus?) 
-# TODO? (BB value?  =  PPL( h | t ) / PPL(t / t) ) 
+# Consider
+# - do we need to normalize PMI? No.
+# - Minus value is normalized, right? (right. per-word)
+#   (Is this always a linear-relation to PMI?): hmm. Not sure. check.
 
-use warnings; 
-use strict; 
-use POSIX qw(_exit); 
-use Benchmark qw(:all); 
+use warnings;
+use strict;
+#use POSIX qw(_exit);
+use Benchmark qw(:all);
 #use proto_condprob qw(:DEFAULT set_num_thread $DEBUG $APPROXIMATE_WITH_TOP_N_HITS export_hash_to_file $SOLR_URL); 
 use condprob qw(:DEFAULT set_num_thread $DEBUG export_hash_to_file $SOLR_URL); 
 
-my $lambda = 0.5; 
-my $TRAINFILE = "./testdata/English_dev.xml"; 
-my $TESTFILE = "./testdata/English_test.xml"; 
-my $TEMP_DIR = "./temp"; 
-die "Usage: needs two argument.\n\">perl runner.pl 3 train\" will pick train data  id 3 and run it. [train/test]" unless ($ARGV[1]); 
+my $lambda = 0.3;
+#my $TRAINFILE = "./testdata/English_dev.xml";
+#my $TESTFILE = "./testdata/English_test.xml";
+my $TEMP_DIR = "./temp";
+die "Usage: needs three arguments.\n\">perl runner.pl rte_filename start_num end_num\"\n perl runner.pl ./testdata/English_dev.xml 0 800" unless ($ARGV[2]);
 
-my $RTEFILE; 
-if ((lc($ARGV[1]) eq "train")  or (lc($ARGV[1]) eq "dev") )
-{
-    $RTEFILE = $TRAINFILE; 
-}
-elsif((lc($ARGV[1]) eq "test"))
-{
-    $RTEFILE = $TESTFILE; 
-}
-else
-{
-    die "unknown ARGV[1]: $ARGV[1]\n"; 
-}
+my $RTEFILE = $ARGV[0];
+die "unable to open file: $RTEFILE" unless (-r $RTEFILE);
+
+my $START_ID = $ARGV[1] + 0;
+my $END_ID = $ARGV[2] + 0;
+
+die "start id out of bounds" if ($START_ID < 1 or $START_ID > 800);
+die "end id out of bounds" if ($END_ID < 1 or $END_ID > 800);
+die "end id must be bigger than start" if ($END_ID < $START_ID);
 
 # PARAMETERS to set (for proto_condprob.pm) 
 our $DEBUG=0; # well, turn it on for quality check. 
-set_num_thread(4);  
-our $SOLR_URL = "http://localhost:9911/solr"; 
-our $APPROXIMATE_WITH_TOP_N_HITS=4000; 
+set_num_thread(2);
+our $SOLR_URL = "http://localhost:9911/solr";
+our $APPROXIMATE_WITH_TOP_N_HITS=4000;
 
 # time in 
-my $t0 = Benchmark->new; 
+my $t0 = Benchmark->new;
 
 # read data 
-my ($t_aref, $h_aref, $d_aref) = read_rte_data($RTEFILE); 
+my ($t_aref, $h_aref, $d_aref) = read_rte_data($RTEFILE);
 #for(my $i=0; $i < scalar(@$t_aref); $i++)
 #{
 #    print "id: ", ($i+1), "\t", $d_aref->[$i] ,"\n"; 
@@ -53,74 +50,72 @@ my ($t_aref, $h_aref, $d_aref) = read_rte_data($RTEFILE);
 #    print "H: ", $h_aref->[$i]; 
 #}
 
-# now select one 
-my $id = $ARGV[0] - 1; 
-die "something wrong with id: $id\n" unless ($id >= 0); 
+# now select one
+for (my $pair_id = $START_ID; $pair_id <= $END_ID; $pair_id++)
+{
+  my $id = $pair_id - 1; # starting from 0.
 
-my $text = call_splitta($t_aref->[$id]); 
-my $hypo = call_splitta($h_aref->[$id]); 
+  my $text = call_splitta($t_aref->[$id]);
+  my $hypo = call_splitta($h_aref->[$id]);
+  print STDERR "Processing id $id;\n";
+  my ($collection_p_h, $model_p_h, $model_p_h_given_t, $h_words, $h_sents) = condprob_h_given_t($hypo, $text, $lambda, "./models/collection/collection.model", "./models/document");
 
-#my ($bb, $pmi, $P_pw_h_given_t, $P_h_given_t_minus_h, $tlen, $hlen, $P_h_given_t, $P_t, $P_h, $weighted_href) = P_h_t_multithread_index($hypo, $text, 0.5, "./models/collection/collection.model", "./models/document", "./models_index");
+  #$| = 1;
 
-#my ($bb, $pmi, $P_pw_h_given_t, $P_h_given_t_minus_h, $tlen, $hlen, $P_h_given_t, $P_t, $P_h, $weighted_href) = P_h_t_index($hypo, $text, 0.5, "./models/collection/collection.model", "./models/document");
+  my $pmi = "pmi(TBD)";
+  my $bb = "bb_val(TBD)";
+  my $ppl_minus = "minus(TBD)";
+  my $target_ppl = calc_ppl($model_p_h_given_t, $h_words, $h_sents);
+  my $uncond_ppl = calc_ppl($model_p_h, $h_words, $h_sents);
+  $ppl_minus = $uncond_ppl - $target_ppl;
+  $pmi = $model_p_h_given_t - $model_p_h;
+  print "$pair_id|GOLD:$d_aref->[$id]|, $bb, $pmi, $target_ppl, $ppl_minus, $h_words, $h_sents, $model_p_h_given_t, $model_p_h\n";
 
-my ($collection_p_h, $model_p_h, $model_p_h_given_t, $h_words, $h_sents) = condprob_h_given_t($hypo, $text, $lambda, "./models/collection/collection.model", "./models/document");
-
-#$| = 1; 
-#print "$ARGV[0]|GOLD:$d_aref->[$id]|, $bb, $pmi, $P_pw_h_given_t, $P_h_given_t_minus_h, $tlen, $hlen, $P_h_given_t, $P_t, $P_h,\n";  
-
-my $pmi = "pmi(TBD)"; 
-my $bb = "bb_val(TBD)"; 
-my $minus = "minus(TBD)"; 
-my $ppl = calc_ppl($model_p_h_given_t, $h_words, $h_sents); 
-#print "$ARGV[0]|GOLD:$d_aref->[$id]|, $bb, $pmi, $P_pw_h_given_t, $P_h_given_t_minus_h, $tlen, $hlen, $P_h_given_t, $P_t, $P_h,\n";  
-
-print "$ARGV[0]|GOLD:$d_aref->[$id]|, $bb, $pmi, $ppl, $minus, $h_words, $h_sents, $model_p_h_given_t, $model_p_h\n";  
-
+}
 # time stamp
-my $t1 = Benchmark->new; 
-my $td = timediff($t1, $t0); 
-print STDERR "the code took:", timestr($td), "\n"; 
-exit(0); 
+my $t1 = Benchmark->new;
+my $td = timediff($t1, $t0);
+print STDERR "the code took:", timestr($td), "\n";
+#exit(0);
 ###
 ###
 ###
 
 # call splitta for tokenization ... 
-sub call_splitta 
-{
-    print STDERR "tokenization ..."; 
-    my $s = shift; 
+# sub call_splitta 
+# {
+#     print STDERR "tokenization ..."; 
+#     my $s = shift; 
 
-    # write a temp file
-    my $file = $TEMP_DIR . "/splitta_input.txt"; 
-    open OUTFILE, ">", $file; 
-    print OUTFILE $s; 
-    close OUTFILE; 
+#     # write a temp file
+#     my $file = $TEMP_DIR . "/splitta_input.txt"; 
+#     open OUTFILE, ">", $file; 
+#     print OUTFILE $s; 
+#     close OUTFILE; 
     
-    # my $splitted_output = "$temp_dir" . $file_basename . ".splitted"; 
-    `python ./splitta/sbd.py -m ./splitta/model_nb -t -o $TEMP_DIR/splitted.txt $file 2> /dev/null`;
-    print STDERR " done\n"; 
+#     # my $splitted_output = "$temp_dir" . $file_basename . ".splitted"; 
+#     `python ./splitta/sbd.py -m ./splitta/model_nb -t -o $TEMP_DIR/splitted.txt $file 2> /dev/null`;
+#     print STDERR " done\n"; 
 
-    open INFILE, "<", $TEMP_DIR . "/splitted.txt"; 
-    my $splitted=""; 
-    while(<INFILE>)
-    {
-	# NOTE: this process must be the same as training data generated
-	# in gigaword_split_file.pl 
+#     open INFILE, "<", $TEMP_DIR . "/splitted.txt"; 
+#     my $splitted=""; 
+#     while(<INFILE>)
+#     {
+# 	# NOTE: this process must be the same as training data generated
+# 	# in gigaword_split_file.pl 
 
-	# fixing tokenization error of Splitta (the end of sentence) 
-	# case 1) Period (\w.$) at the end  -> (\w .$) 
-	s/\.$/ \. /; 
-	# case 2) Period space quote (\w. " $) at the end. -> (\w . " $) 
-	s/\. " $/ \. " /;
+# 	# fixing tokenization error of Splitta (the end of sentence) 
+# 	# case 1) Period (\w.$) at the end  -> (\w .$) 
+# 	s/\.$/ \. /; 
+# 	# case 2) Period space quote (\w. " $) at the end. -> (\w . " $) 
+# 	s/\. " $/ \. " /;
 
-	$splitted .= $_; 
-    }
-    close INFILE; 
+# 	$splitted .= $_; 
+#     }
+#     close INFILE; 
 
-    return lc($splitted); 
-}
+#     return lc($splitted); 
+# }
 
 
 # reading EOP RTE file. 
